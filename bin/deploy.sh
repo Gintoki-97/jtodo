e879ac0f6d42e7fb290253883b9aeadf5b58808e
Gintoki <1091729844@qq.com>
2018-10-05 17:36:39 +0800

    Fixed the deploy shell bug

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -13,12 +13,12 @@ then
     if [ $? -eq 0 ]
 
     then
-        source /home/jesus/developkit/service/tomcat/jtodo/t1/bin/shutdown.sh
+        sh /home/jesus/developkit/service/tomcat/jtodo/t1/bin/shutdown.sh
         
         rm -rf /home/jesus/developkit/service/tomcat/jtodo/t1/webapps/*
         cp /home/jesus/developkit/repository/git/jtodo/src/web/target/jtodo*.war /home/jesus/developkit/service/tomcat/jtodo/t1/webapps/jtodo.war
             
-        source /home/jesus/developkit/service/tomcat/jtodo/t1/bin/startup.sh
+        sh /home/jesus/developkit/service/tomcat/jtodo/t1/bin/startup.sh
     else
         echo "Git pull -r failed"
     fi
