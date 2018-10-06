bcd67538b2bba76f5d3baba1e8b96029c22dd0fb
Gintoki-97 <1091729844@qq.com>
2018-10-06 19:49:06 +0800

    Add the test method of authentication

--- src/api/src/main/java/cn/gin/module/service/UserService.java
+++ src/api/src/main/java/cn/gin/module/service/UserService.java
@@ -2,11 +2,39 @@ package cn.gin.module.service;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserService {
 
+    /**
+     * Return whether current user is authenticated
+     *
+     * @return Whether current user is authenticated
+     */
     public static boolean isAuthenticated() {
 
         return SecurityContextHolder.getContext().getAuthentication().isAuthenticated();
+    }
+
+    /**
+     * Get current authenticated user
+     *
+     * @return {@link UserDetails}
+     */
+    public static UserDetails getCurrentUser() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = null;
+
+        if (authentication.isAuthenticated()) {
+
+            try {
+                userDetails = (UserDetails) authentication.getPrincipal();
+            } catch (Exception exception) {
+                // Nothing
+            }
+        }
+
+        return userDetails;
     }
 }
--- src/api/src/main/java/cn/gin/module/web/controller/SiteController.java
+++ src/api/src/main/java/cn/gin/module/web/controller/SiteController.java
@@ -3,6 +3,9 @@ package cn.gin.module.web.controller;
 import cn.gin.common.Constants;
 import cn.gin.common.util.JsonObject;
 import cn.gin.module.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SiteController {
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @GetMapping({Constants.Symbol.EMPTY, Constants.Path.CTRL_SITE_INDEX})
     public String index() {
 
-        return JsonObject.ok(String.valueOf(UserService.isAuthenticated()));
+        UserDetails userDetails = UserService.getCurrentUser();
+        JsonObject json = new JsonObject(true, "Request success");
+        json.addParam("user", userDetails);
+
+        return json.toJson();
     }
 }
