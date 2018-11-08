4cf2486b642ce1b2e661d19d7a2344d8aa9eea56
Jesus Yang <jesusyang@augmentum.com.cn>
2018-11-08 12:44:25 +0800

    Refined the project structure

--- jtodo/src/main/java/cn/gin/App.java
+++ jtodo/src/main/java/cn/gin/App.java
@@ -2,8 +2,6 @@ package cn.gin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Application starter, used to run as a jar package
